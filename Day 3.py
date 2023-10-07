5842e7a6f139f4861da89a95f5d6f9c3ee1540ca
Sai Khairnar <85782407+saikhairnar20023@users.noreply.github.com>
2023-10-07 12:33:18 +0530

    Add files via upload

---
+++ Day 3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  7 12:32:17 2023
+
+@author: Sai Khairnar
+"""
+
+import pandas  # This is an example of external module
+import hashlib  # This is an example of built in module
+
+print("Hi")
+
+# Dont worry about how to use these modules just yet!
+pandas.read_csv("one.csv")
+m = hashlib.sha256()
