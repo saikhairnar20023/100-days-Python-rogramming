f9f3f24d7c6c9c18a662e8e1fd903b3c5ce4b812
Sai Khairnar <85782407+saikhairnar20023@users.noreply.github.com>
2023-10-07 12:48:16 +0530

    Add files via upload

---
+++ Day 6.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  7 12:43:50 2023
+
+@author: Sai khainar
+"""
+
+a = complex(8, 2)
+b = True
+c = "Harry"
+d = None
+print(a)
+print(b)
+a1 = 9
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
