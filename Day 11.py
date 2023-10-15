1db5f41954cdb9bf88ca3e6967dc4b34d7ee6370
Sai Khairnar <85782407+saikhairnar20023@users.noreply.github.com>
2023-10-15 15:39:32 +0530

    Add files via upload

---
+++ Day 11.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 15 15:33:38 2023
+
+@author: Sai Khairnar
+"""
+
+import time
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+timestamp = time.strftime('%H')
+print(timestamp)
+timestamp = time.strftime('%M')
+print(timestamp)
+timestamp = time.strftime('%S')
+print(timestamp)
+# https://docs.python.org/3/library/time.html#time.strftime
