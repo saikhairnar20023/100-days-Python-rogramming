22bf06aee32196824740d5317f90601974dc46c3
Sai Khairnar <85782407+saikhairnar20023@users.noreply.github.com>
2023-10-18 23:03:04 +0530

    Add files via upload

---
+++ Day 14.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 23:01:54 2023
+
+@author: Sai khairnar
+"""
+
+i = int(input("Enter the number: "))
+print(i)
+while(i<=38):
+  i = int(input("Enter the number: "))
+  print(i)
+
+print("Done with the loop")
+
+# count = 5
+# while (count > 0):
+#   print(count)
+#   count = count - 1
+# else:
+#   print("I am inside else")
+
+# do {
+  # loop body;
+# }while(condition);
