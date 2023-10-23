3d179cbfce7c58e686b32ac907e882a293bfe2d6
Sai Khairnar <85782407+saikhairnar20023@users.noreply.github.com>
2023-10-23 16:05:02 +0530

    Add files via upload

---
+++ Day 16.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 23 16:02:46 2023
+
+@author: Sai Khairnar
+"""
+
+def calculateGmean(a, b):
+  mean = (a*b)/(a+b)
+  print(mean)
+
+def isGreater(a, b):
+  if(a>b):
+    print("First number is greater")
+  else:
+    print("Second number is greater or equal")
+
+def isLesser(a, b):
+  pass
+  
+
+a = 9
+b = 8
+isGreater(a, b)
+calculateGmean(a, b)
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
+c = 8
+d = 74
+isGreater(c, d)
+calculateGmean(c, d)
+# gmean2 = (c*d)/(c+d)
+# print(gmean2)
---
+++ day 15.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 23 16:02:20 2023
+
+@author: Sai Khairnar
+"""
+
+# for i in range(12):
+#   if(i == 10):
+#     print("Skip the iteration")
+#     continue
+#   print("5 X", i, "=", 5 * i)
+  
+i = 0
+while True:
+  print(i)
+  i = i + 1
+  if(i%100 == 0):
+    break
